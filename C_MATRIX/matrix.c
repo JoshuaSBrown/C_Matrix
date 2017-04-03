a685a012031ff7d03e34407f0ee7a89fc3262998
Joshua Brown <joshbro42867@yahoo.com>
2017-04-03 15:11:07 -0600

    First Commit

---
+++ C_MATRIX/matrix.c
@@ -0,0 +1,560 @@
+/******************************************************************************/
+/*                                                                            *
+ *          Joshua Brown                                                      *
+ *          Scientific Computing                                              *
+ *          matrix.c function                                                 *
+ *                                                                            *
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "matrix.h"
+
+/******************************************************************************
+ * Internal Library Functions                                                 *
+ ******************************************************************************/
+static inline void _hardcrash(void){
+#ifdef _HARDCRASH_
+  exit(1);
+#endif
+}
+
+static inline float _add(const float val1, const float val2){
+  return val1+val2;
+}
+
+static inline float _sub(const float val1, const float val2){
+  return val1-val2;
+}
+
+static inline float _mul(const float val1, const float val2){
+  return val1*val2;
+}
+
+static inline float _div(const float val1, const float val2){
+  return val1/val2;
+}
+
+static inline float _val(const float val1, const float val2){
+  return 0.0+val2;
+}
+
+static inline float _getE(const matrix * mat, const int r, const int c){
+  return mat->data[(r)*mat->cols+c];
+}
+
+static inline void _setE(matrix * mat  ,
+	                      const int r    ,
+												const int c    ,
+	                      const float val)
+{
+  mat->data[(r)*mat->cols+c] = val;
+}
+
+static inline void _addE(matrix * mat   ,
+	                       const int r    ,
+												 const int c    ,
+												 const float val)
+{
+  _setE(mat,r,c,_getE(mat,r,c)+val);
+}
+
+static inline void _manipulate(matrix * mat              ,
+	                             const float val           ,
+															 float (*func)(float,float)){
+  for(int i=0;i<mat->rows;i++){
+    for(int j=0;j<mat->cols;j++){
+      _setE(mat,i,j,(*func)(_getE(mat,i,j),val));
+    }
+  }
+}
+
+/******************************************************************************
+ * User Library Functions                                                     *
+ ******************************************************************************/
+/* Creator                                                                    */
+matrix * newMatrix(int r, int c){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(r<1){
+		fprintf(stderr,"ERROR r is less than 1 in newMatrix, a matrix must have "
+                   "rows greater than 0\n");
+    _hardcrash();
+		return NULL;
+	}
+	if(c<1){
+		fprintf(stderr,"ERROR c is less than 1 in newMatrix, a matrix must have "
+                   "cols greater than 0\n");
+    _hardcrash();
+		return NULL;
+	}
+#endif
+	matrix * mat = (matrix *) malloc(sizeof(struct _matrix)+sizeof(float)*r*c);
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in newMatrix\n");
+    _hardcrash();
+		return NULL;
+	}
+#endif
+	mat->rows=r;
+	mat->cols=c;
+
+	/* Initialize matrix so each of the elements
+     * is 0 */
+	for(int i=0;i<r;i++){
+		for(int j=0;j<c;j++){
+			_setE(mat,i,j,0);
+		}
+	}
+	return mat;
+}
+
+/* Destructor                                                                 */
+int deleteMatrix(matrix ** mat){
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in deleteMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+	free(*mat);
+	*mat = NULL;
+	return 0;
+}
+
+/* Displayers                                                                 */
+int printMatrix(const matrix * mat){
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in printMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+  printf("\n*****************************************\n");
+	printf("Cols %d Rows %d\n\n",mat->cols,mat->rows);
+
+	for(int j=0;j<mat->cols;j++){
+		printf("\t Col %6d\t",j);
+	}
+	printf("\n");
+
+	for(int i=0;i<mat->rows;i++){
+		printf("Row %d\t",i);
+		for(int j=0;j<mat->cols;j++){
+			printf("%8f\t",_getE(mat,i,j));
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+	return 0;
+}
+
+int printIntMatrix(const matrix * mat){
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in printIntMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+	printf("\n*****************************************\n");
+	printf("Cols %d Rows %d\n\n",mat->cols,mat->rows);
+
+	for(int i=0;i<mat->rows;i++){
+		for(int j=0;j<mat->cols;j++){
+			printf("%3d ",(int) _getE(mat,i,j));
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
+/* Getters                                                                    */
+int getRowsMatrix(const matrix * mat){
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in getRowsMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+	return mat->rows;
+}
+
+int getColsMatrix(const matrix * mat){
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in getColsMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+	return mat->cols;
+}
+
+int getTotalElemsMatrix(const matrix * mat){
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in getTotalElemsMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+	return mat->cols*mat->rows;
+}
+
+int sumAllElemsMatrix(const matrix * mat,float * sum){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in sumAllElemsMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(sum==NULL){
+		fprintf(stderr,"ERROR sum is NULL in sumAllElemsMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+  *sum = 0.0;
+	for(int i=0;i<mat->rows;i++){
+		for(int j=0;j<mat->cols;j++){
+			*sum += _getE(mat,i,j);
+		}
+	}
+	return 0;
+}
+
+int getElemMatrix(const matrix * mat, const int r, const int c,float * val){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in getElemMatrix\n");
+    _hardcrash();
+    return -1.0;
+	}
+	if(val==NULL){
+		fprintf(stderr,"ERROR val is NULL in getElemMatrix\n");
+    _hardcrash();
+    return -1.0;
+	}
+	if(r<0){
+		fprintf(stderr,"ERROR r is less than 0 in getElemMatrix\n");
+    _hardcrash();
+    return -1.0;
+	}
+	if(c<0){
+		fprintf(stderr,"ERROR c is less than 0 in getElemMatrix\n");
+    _hardcrash();
+    return -1.0;
+	}
+	if(r>=mat->rows){
+		fprintf(stderr,"ERROR r greater than rows in matrix getElemMatrix\n");
+    _hardcrash();
+    return -1.0;
+	}
+	if(c>=mat->cols){
+		fprintf(stderr,"ERROR c greater than cols in matrix getElemMatrix\n");
+    _hardcrash();
+    return -1.0;
+	}
+#endif
+	*val = _getE(mat,r,c);
+	return 0;
+}
+
+/* Setters                                                                    */
+int setElemMatrix(matrix * mat, const int r, const int c, const float val){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in setElemMatrix\n");
+		_hardcrash();
+		return -1;
+	}
+	if(r<0){
+		fprintf(stderr,"ERROR row is less than 0 in setElemMatrix\n");
+    _hardcrash();
+		return -1;
+	}
+	if(c<0){
+		fprintf(stderr,"ERROR col is less than 0 in setElemMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(r>=mat->rows){
+		fprintf(stderr,"ERROR row greater than rows in matrix setElemMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(c>=mat->cols){
+		fprintf(stderr,"ERROR col greater than cols in matrix setElemMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+	_setE(mat,r,c,val);
+
+	return 0;
+}
+
+int setAllMatrix(matrix * mat, const float val){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in setAllMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+  _manipulate(mat,val,&_val);
+	return 0;
+}
+
+int addAllMatrix(matrix * mat, const float val){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in addAllMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+  _manipulate(mat,val,&_add);
+	return 0;
+}
+
+int subAllMatrix(matrix * mat, const float val){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in subAllMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+  _manipulate(mat,val,&_sub);
+	return 0;
+}
+
+int mulAllMatrix(matrix * mat, const float val){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in mulAllMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+  _manipulate(mat,val,&_mul);
+	return 0;
+}
+
+int divAllMatrix(matrix * mat, const float val){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(mat==NULL){
+		fprintf(stderr,"ERROR mat is NULL in divAllMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+  _manipulate(mat,val,&_div);
+	return 0;
+}
+
+/* Manipulators                                                               */
+
+/* start_row, end_row, start_col and end_col can be any
+ * value between and including 0 and row-1, col-1 of the
+ * smallest of the two matrices */
+int copyMatrix(const int start_row_elem   ,
+               const int end_row_elem     ,
+               const int start_col_elem   ,
+               const int end_col_elem     ,
+               const matrix * mat_orig    ,
+               matrix ** mat_copy    ){
+
+	int col_length = end_col_elem-start_col_elem;
+	int row_length = end_row_elem-start_row_elem;
+#ifdef _ERROR_CHECKING_ON_
+	if(mat_copy == NULL){
+		fprintf(stderr,"ERROR mat_copy is NULL in copyMatrix\n");
+    _hardcrash();
+		return -1;
+  }
+	if(*mat_copy == NULL){
+		fprintf(stderr,"ERROR *mat_copy is NULL in copyMatrix\n");
+    _hardcrash();
+		return -1;
+  }
+	if(mat_orig == NULL){
+		fprintf(stderr,"ERROR mat_orig is NULL in copyMatrix\n");
+    _hardcrash();
+    return -1;
+  }
+	if(start_row_elem>=end_row_elem){
+		fprintf(stderr,"ERROR start_row_elem is larger than end_row in "
+                   "copyMatrix\n");
+    _hardcrash();
+		return -1;
+	}
+	if(start_col_elem>=end_col_elem){
+		fprintf(stderr,"ERROR start_col_elem  larger than end_col in "
+                   "copyMatrix\n");
+     _hardcrash();
+    return -1;
+	}
+	if(start_row_elem<0){
+		fprintf(stderr,"ERROR start_row_elem  is less than 0 in "
+                   "copyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(start_col_elem<0){
+		fprintf(stderr,"ERROR start_col_elem  is less than 0 in "
+                   "copyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(end_row_elem>mat_orig->rows){
+		fprintf(stderr,"ERROR end_row_elem  extends past the row length of the "
+                   "original matrix in copyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(end_col_elem>mat_orig->cols){
+		fprintf(stderr,"ERROR end_col_elem  is larger than the column length of the"
+                   "original matrix in copyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(row_length>(*mat_copy)->rows){
+		fprintf(stderr,"ERROR rows to be copied extend past the row length of "
+                   "mat_copy in copyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(col_length>(*mat_copy)->cols){
+		fprintf(stderr,"ERROR columns extend past the column length of mat_copy in "
+                   "copyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+	for(int i=start_row_elem;i<end_row_elem;i++){
+		for(int j=start_col_elem;j<end_col_elem;j++){
+			const float val = _getE(mat_orig,i+start_row_elem,j+start_col_elem);
+			_setE((*mat_copy),i,j,val);
+		}
+	}
+
+	return 0;
+}
+
+/* Function multiplies two matrices together to get a third matrix
+ * then adds to what ever values were already stored in matrix_c:
+ * c = c+a*b
+ */
+int multiplyAddMatrix(matrix * c, const matrix * a, const matrix * b){
+#ifdef _ERROR_CHECKING_ON_
+	if(a->cols!=b->rows){
+		fprintf(stderr,"ERROR a matrix does not have the number of columns as b has"
+                   " rows in multiplyAddMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(a->rows!=c->rows){
+		fprintf(stderr,"ERROR c matrix does not have the number of rows as a in "
+                   "multiplyAddMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(b->cols!=c->cols){
+		fprintf(stderr,"ERROR c matrix does not have the number of cols as b in "
+                   "multiplyAddMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+	for(int k=0;k<a->cols;k++){
+		for(int i=0;i<a->rows;i++){
+      float val = 0.0;
+			for(int j=0;j<a->cols;j++){
+				val+=_getE(a,i,j)*_getE(b,j,k);
+			}
+			_addE(c,i,k,val);
+		}
+	}
+
+	return 0;
+}
+
+/* Function multiplies two matrices together to get a third matrix
+ * Such that:
+ * c = a*b
+ */
+int multiplyMatrix(matrix * c, const matrix * a, const matrix * b){
+
+#ifdef _ERROR_CHECKING_ON_
+	if(a->cols!=b->rows){
+		fprintf(stderr,"ERROR a matrix does not have the number of columns as b has"
+                   " rows in multiplyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(a->rows!=c->rows){
+		fprintf(stderr,"ERROR c matrix does not have the number of rows as a in "
+                   "multiplyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+	if(b->cols!=c->cols){
+		fprintf(stderr,"ERROR c matrix does not have the number of cols as b in "
+                   "multiplyMatrix\n");
+    _hardcrash();
+    return -1;
+	}
+#endif
+
+	for(int k=0;k<a->cols;k++){
+		for(int i=0;i<a->rows;i++){
+			float val = 0.0;
+			for(int j=0;j<a->cols;j++){
+				val+=_getE(a,i,j)*_getE(b,j,k);
+			}
+			_addE(c,i,k,val);
+		}
+	}
+
+	return 0;
+}
+
+int generateFileMatrix(char * filename, const matrix * mat){
+
+#ifdef _ERROR_CHECKING_ON_
+  if(mat==NULL){
+    fprintf(stderr,"ERROR mat is NULL in generateFileMatrix\n");
+    _hardcrash();
+    return -1;
+  }
+#endif
+	FILE * fid;
+	fid = fopen(filename,"w");
+
+	for(int j=0;j<mat->cols;j++){
+		for(int i=0;i<mat->rows;i++){
+			fprintf(fid,"%6d ",(int)_getE(mat,i,j));
+		}
+		fprintf(fid,"\n");
+	}
+	fclose(fid);
+	return 0;
+}
---
+++ C_MATRIX/matrix.h
@@ -0,0 +1,147 @@
+/**************************************/
+/*
+ *          Joshua Brown
+ *          Scientific Computing
+ *          matrix.h function
+ *
+ */
+/**************************************/
+
+#include <stdbool.h>
+
+#ifndef _MIDTERM_MATRIX_H_
+#define _MIDTERM_MATRIX_H_
+
+/* Here rows and cols describe the height and width of the matrix. The array  *
+ * data contains the data values held within the matrix                       */
+struct _matrix {
+	int   rows;
+	int   cols;
+	float data[0];
+};
+
+typedef struct _matrix matrix;
+
+/* Creator                                                                    */
+/* Create a new matrix of intergers */
+matrix * newMatrix(int rows, int cols);
+
+/* Destructor                                                                 */
+/* free memory used when the matrix was created */
+int deleteMatrix(matrix ** mat);
+
+/* Displayers                                                                 */
+/* Print the contents of a matrix, the return value is given as:              *
+ * -1 - if the matrix was NULL                                                *
+ *  0 - if success                                                            */
+int printMatrix(const matrix * mat);
+
+/* Print the contents of a matrix in compressed form. Used to convert floating*
+ * point numbers to an interger and then prints intergers with place holders  *
+ * corresponding to two significant digits. The return value is given as:     *
+ * -1 - if the matrix was NULL                                                *
+ *  0 - if success                                                            */
+int printIntMatrix(const matrix * mat);
+
+/* Getters                                                                    */
+/* Gets the total rows of the matrix the return value is given as:            *
+ * >0 - greater than 0 where the returned values indicates the number of rows *
+ * -1 - indicates the matrix was NULL                                         */
+ int getRowsMatrix(const matrix * mat);
+
+ /* Gets the total columns of the matrix, where the return values are:        *
+  * >0 - greater than 0 for the number of columns                             *
+	* -1 - indicates the matrix was NULL                                        */
+int getColsMatrix(const matrix * mat);
+
+/* Function sums all the elements in the matrix and returns the total, the    *
+ * return values is given as:                                                 *
+ *  0 - indicates success                                                     *
+ * -1 - indicates the matrix was NULL or sum was NULL                         */
+int sumAllElemsMatrix(const matrix * mat, float * sum);
+
+/* Determines the total number of elements in the matrix where the return     *
+ * values is given as:                                                        *
+ * >0 - greater than 0 indicates the total number of elements                 *
+ * -1 - indicates the matrix was NULL                                         */
+int getTotalElemsMatrix(const matrix * mat);
+
+/* Grab an element at row r and column c and place it in the pointer val, the *
+ * return value is given as:                                                  *
+ * -1 - indicates r and c were out of bounds or that mat or val were NULL     *
+ *  0 - success                                                               */
+int getElemMatrix(const matrix * mat, const int r, const int c, float * val);
+
+/* Setters                                                                    */
+/* Function changes the elements at row r and column c to the value val, the  *
+ * return value is given as:                                                  *
+ * -1 - if r and c is out of bounds or if the matrix is NULL                  *
+ *  0 - on success                                                            */
+int setElemMatrix(matrix * mat, const int r, const int c, const float val);
+
+/* Function sets every element in the matrix to the value given by val, the   *
+ * return value is given as:                                                  *
+ * -1 - indicates NULL matrix                                                 *
+ *  0 - success                                                               */
+int setAllMatrix(matrix * mat, const float val);
+
+/* Function adds to every element in the matrix mat the value given by val,   *
+ * the return value is given as:                                              *
+ * -1 - indicates mat is NULL                                                 *
+ *  0 - success                                                               */
+int addAllMatrix(matrix * mat, const float val);
+
+/* Function subtracts from every element the value contained in val, the      *
+ * return value is given as:                                                  *
+ * -1 - indicates the matrix mat was NULL                                     *
+ *  0 - success                                                               */
+int subAllMatrix(matrix * mat, const float val);
+
+/* Function multiplies every element in the matrix mat with the value val, the*
+ * return value is given as:                                                  *
+ * -1 - indicates matrix mat is NULL                                          *
+ *  0 - success                                                               */
+int mulAllMatrix(matrix * mat, const float val);
+
+/* Function divides every element in the matrix mat with the value val, the   *
+ * return value is given as:                                                  *
+ * -1 - indicates matrix mat is NULL                                          *
+ *  0 - success                                                               */
+int divAllMatrix(matrix * mat, const float val);
+
+/* Manipulators                                                               */
+/* Copies the elements between and including
+ * start_row to end_row
+ * start_col to end_col
+ * to a separate matrix the values are placed in
+ * the new matrix starting at row and col (0,0) */
+int copyMatrix(const int start_row_elem ,
+               const int end_row_elem   ,
+               const int start_col_elem ,
+               const int end_col_elem   ,
+               const matrix * mat_orig  ,
+               matrix ** mat_copy    );
+
+/* matrix multiplication is executed such that the output is stored in c.     *
+ * However, the elements in c are not overwritted but the elements are added  *
+ * to such that you get:                                                      *
+ * c = c+a*b                                                                  *
+ * The return value is given as:                                              *
+ * -1 - If matrices are of different sizes or are NULL                        *
+ *  0 - If success                                                            */
+int multiplyAddMatrix(matrix * c, const matrix * a, const matrix * b);
+
+/* matrix multiplication is executed such that the output is stored in c. i.e.*
+ * c = a*b                                                                    *
+ * Wheret the return value is given as:                                       *
+ * -1 - if the matrices are NULL or are of different sizes                    *
+ *  0 - if success                                                            */
+int multiplyMatrix(matrix *c, const matrix * a, const matrix * b);
+
+/* Function generates a file with the contents of the matrix, the name of the *
+ * file is specified through filename will return:                            *
+ * -1 - if the matrix mat is NULL                                               *
+ *  0 - success                                                               */
+int generateFileMatrix(char * filename, const matrix * mat);
+
+#endif
